97caa18e147d41b8f0507c4abe3c2d0cd3ef97e5
Carl Johnson <cmjohnson@pbs.org>
2016-03-30 11:01:35 -0400

    UCD: Add look up by codepoint

--- cmd/ucd/ucd.go
+++ cmd/ucd/ucd.go
@@ -7,32 +7,59 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 
 	"github.com/carlmjohnson/unicodechess/unicodedata"
 )
 
-var input io.Reader = os.Stdin
-
-func init() {
+func main() {
+	hcp := flag.String("codepoint", "", "Display data for hex codepoint")
+	dcp := flag.String("dec-codepoint", "", "Display data for decimal codepoint")
 	flag.Parse()
-	if flag.NArg() == 0 {
+
+	if *hcp != "" {
+		byCodepoint(*hcp, 16)
 		return
 	}
+
+	if *dcp != "" {
+		byCodepoint(*dcp, 10)
+		return
+	}
+
+	if flag.NArg() == 0 {
+		byCharacter(os.Stdin)
+		return
+	}
+
 	var buf bytes.Buffer
 	for _, s := range flag.Args() {
 		buf.WriteString(s)
 	}
-	input = &buf
+	byCharacter(&buf)
 }
 
-func main() {
+func byCharacter(input io.Reader) {
 	s := bufio.NewScanner(input)
 	s.Split(bufio.ScanRunes)
 
 	for s.Scan() {
 		s := s.Text()
 		r := []rune(s)[0]
-		name := unicodedata.UCD[r]
-		fmt.Printf("0x%04X\t%s\t%s\n", r, s, name)
+		print(r)
 	}
+}
+
+func byCodepoint(input string, base int) {
+	cp, err := strconv.ParseInt(input, base, 32)
+	if err != nil {
+		fmt.Printf("Could not process codepoint, base-%d: %v\n", base, err)
+		os.Exit(1)
+	}
+	print(rune(cp))
+}
+
+func print(r rune) {
+	name := unicodedata.UCD[r]
+	fmt.Printf("0x%04X\t%s\t%s\n", r, string(r), name)
 }
